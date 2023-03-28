d3dc69800556c83c3e823c780ead45bcce0dc1d2
Manesh <manisht@fin-techsolutions.com>
2023-03-28 13:52:57 +0530

    code

---
+++ src/main/java/com/example/com/GithubCicdActionsApplication.java
@@ -0,0 +1,30 @@
+package com.example.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to jenkins";
+    }
+
+//    echo "# github-actions-repo" >> README.md
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M main
+//    git remote add origin https://github.com/Manesh04/github-actions-repo.git
+//    git push -u origin main
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCicdActionsApplication.class, args);
+    }
+
+}
